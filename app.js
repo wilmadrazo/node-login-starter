07e58efa665c0e169d56af4c74ec48d527233520
Wilford Madrazo <wil.jacobmadrazo@gmail.com>
2020-04-20 03:22:55 +0800

    Completed app

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const exphbs = require('express-handlebars');
 const handlebars = require('handlebars');
 const bodyParser = require('body-parser');
 const mongoose = require('./models/connection');
+//For sessions
+const session = require('express-session');
+const flash = require('connect-flash');
+const MongoStore = require('connect-mongo')(session);
 
 // Routes imports
 const authRouter = require('./routes/auth');
@@ -40,6 +44,24 @@ app.use(bodyParser.urlencoded({
 app.use(express.static('public'));
 
 // Insert server configuration after this comment
+// Sessions
+app.use(session({
+  secret: 'somegibberishsecret',
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false, maxAge: 1000 * 60 * 60 * 24 * 7 }
+}));
+
+// Flash
+app.use(flash());
+
+// Global messages vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  next();
+});
 
 
 app.use('/', authRouter); // Login/registration routes
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
+const { isPublic, isPrivate } = require('../middlewares/checkAuth');
 
-router.get('/', (req, res) => {
+router.get('/', isPrivate, (req, res) => {
   res.render('home', { pageTitle: 'Home', name: req.session.name } );
 });
 
